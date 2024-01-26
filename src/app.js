3d380a71e8559e7ffe261abbe3f3ff0c96f9136f
Vanessa Ponton <vanessa.ponton@gmail.com>
2024-01-26 10:43:36 +0100

    add put users

--- src/app.js
+++ src/app.js
@@ -12,4 +12,7 @@ app.get("/api/users/:id", movieControllers.getUsersById);
 app.post("/api/movies", movieControllers.postMovie);
 app.post("/api/users", movieControllers.postUser);
 
+app.put("/api/movies/:id", movieControllers.updateMovie);
+app.put("/api/users/:id", movieControllers.updateUser);
+
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -119,6 +119,49 @@ const postUser = (req, res) => {
     });
 };
 
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
 module.exports = {
   getMovies,
   getMovieById,
@@ -126,4 +169,6 @@ module.exports = {
   getUsersById,
   postMovie,
   postUser,
+  updateMovie,
+  updateUser,
 };
